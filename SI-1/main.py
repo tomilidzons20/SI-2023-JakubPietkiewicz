a1cf6f16ce6cd75fb8d93c1e3dcc0b147f30b366
tomilidzons20 <jakubpietkiewicz@outlook.com>
2023-03-04 13:25:56 +0100

    Add exercise 4b,4c

--- SI-1/main.py
+++ SI-1/main.py
@@ -4,45 +4,54 @@ australian = np.loadtxt("../dane/australian.txt", dtype="str")
 australianType = np.loadtxt("../dane/australian-type.txt", dtype="str")
 infoData = np.loadtxt("../dane/_info-data-discrete.txt", dtype="str")
 
+# Zad 3a ==============================================================
+print("\n3a\n")
 # print(australian)
 # print(australianType)
 
-# print("\n3b\n")
-# x, y = np.where(infoData == "australian")
-# print(f"wielkosc klas decyzyjnych = {infoData[int(x)][2]}")
-#
-# print("\n3c\n")
-# x1, y1 = np.where(australianType == "n")
-# for i in x1:
-#     tym = np.array(australian[:, i], dtype='float')
-#     max = np.max(tym)
-#     min = np.min(tym)
-#     print(f"{australianType[i][0]}: max = {max}, min = {min}")
-#
-# print("\n3d\n")
-# for i in range(len(australian[0]) - 1):
-#     print(f"{australianType[i][0]}: uniqueAttributes = {len(np.unique(australian[:, i]))}")
-#
-# print("\n3e\n")
-# for i in range(len(australian[0]) - 1):
-#     print(f"{australianType[i][0]}: uniqueAttributes = {np.unique(australian[:, i])}")
-#
-# print("\n3f\n")
-# x1, y1 = np.where(australianType == "n")
-# wholeSystem = np.array([])
-# for i in x1:
-#     tym = np.array(australian[:, i], dtype='float')
-#     wholeSystem = np.append(wholeSystem, tym)
-#     print(f"{australianType[i][0]}: std = {np.std(tym)}")
-# print(f"Whole system: std = {np.std(wholeSystem)}")
+# Zad 3b ==============================================================
+print("\n3b\n")
+x, y = np.where(infoData == "australian")
+decisionClassSize = int(infoData[int(x)][2])
+print(f"wielkosc klas decyzyjnych = {decisionClassSize}")
 
+# Zad 3c ==============================================================
+print("\n3c\n")
+x1, y1 = np.where(australianType == "n")
+for i in x1:
+    tym = np.array(australian[:, i], dtype='float')
+    max1 = np.max(tym)
+    min1 = np.min(tym)
+    print(f"{australianType[i][0]}: max = {max1}, min = {min1}")
+
+# Zad 3d ==============================================================
+print("\n3d\n")
+for i in range(len(australian[0]) - 1):
+    print(f"{australianType[i][0]}: uniqueAttributes = {len(np.unique(australian[:, i]))}")
+
+# Zad 3e ==============================================================
+print("\n3e\n")
+for i in range(len(australian[0]) - 1):
+    print(f"{australianType[i][0]}: uniqueAttributes = {np.unique(australian[:, i])}")
+
+# Zad 3f ==============================================================
+print("\n3f\n")
+x1, y1 = np.where(australianType == "n")
+wholeSystem = np.array([])
+for i in x1:
+    tym = np.array(australian[:, i], dtype='float')
+    wholeSystem = np.append(wholeSystem, tym)
+    print(f"{australianType[i][0]}: std = {np.std(tym)}")
+print(f"Whole system: std = {np.std(wholeSystem)}")
+
+# Zad 4a ==============================================================
 print("\n4a\n")
 
 # srednie kolumn z n
 x1, y1 = np.where(australianType == "n")
 tab1 = np.zeros(shape=14)
 for i in x1:
-    tab1[i] = np.mean(np.array(australian[:, i], dtype='float')).round(2)
+    tab1[i] = np.mean(np.array(australian[:, i], dtype='float'))
 
 # print(f"{tab1}\n")
 
@@ -57,17 +66,70 @@ for i in x2:
 
 # wypelnienie 10% tabeli znakami "?"
 dataChoice = np.random.choice([True, False], size=australian.shape, p=[0.1, 0.9])
+dataChoice[:, -1] = False
 australian[dataChoice] = "?"
 
-# zapisanie tabeli przed i po wypelnieniu znakow "?" wartosciami srednimi i najczesciej wystepujacymi symbolami
+
+# zapisanie tabeli przed wypelnieniem znakow "?"
+# wartosciami srednimi i najczesciej wystepujacymi symbolami
+# w celu kontroli poprawnosci wykonania zadania
 np.savetxt("file1.txt", australian, fmt="%s")
 
+# zamiana znakow "?" na srednie kolumn
 for i in x1:
     tym = australian[:, i]
     tym[tym == "?"] = tab1[i]
 
+# zamiana znakow "?" na najczesciej wystepujace atrybuty w kolumnie
 for i in x2:
     tym = australian[:, i]
     tym[tym == "?"] = int(tab2[i])
 
+# zapisanie tabeli po wypielnieniu znakow "?"
 np.savetxt("file2.txt", australian, fmt="%s")
+
+# Zad 4b ==============================================================
+print("\n4b\n")
+
+
+# funkcja normalizujaca atrybut do przedzialu
+def interval_calc(a, b, mina, maxa, aiobj):
+    nominator = (float(aiobj) - minai) * (b - a)
+    denominator = maxa - mina
+    res = nominator / denominator + a
+    return str(res)
+
+
+x1, y1 = np.where(australianType == "n")
+
+for i in x1:
+    tym = np.array(australian[:, i], dtype='float')
+    minai = np.min(tym)
+    maxai = np.max(tym)
+    for j in range(decisionClassSize):
+        australian[j, i] = interval_calc(0, 1, minai, maxai, australian[j, i])
+
+np.savetxt("file3.txt", australian, fmt="%s")
+
+# Zad 4c ==============================================================
+print("\n4c\n")
+
+
+# funckja obliczajaca standaryzacje
+def standarization_calc(mean, variance, aiobj):
+    res = (float(aiobj) - mean) / variance
+    return str(res)
+
+
+x1, y1 = np.where(australianType == "n")
+
+# wzor na variance jest w zadaniu nieprawidlowy poniewaz brakuje w nim podzielenia calosci przez number_of_objects
+# dlatego skorzystałem z funkcji wbudowanej w numpy
+# dodatkowo wykorzystałem wzor na srednia z numpy
+for i in x1:
+    meanai = np.mean(np.array(australian[:, i], dtype='float'))
+    varianceai = np.var(np.array(australian[:, i], dtype='float'))
+    for j in range(decisionClassSize):
+        australian[j, i] = standarization_calc(meanai, varianceai, australian[j, i])
+
+np.savetxt("file4.txt", australian, fmt="%s")
